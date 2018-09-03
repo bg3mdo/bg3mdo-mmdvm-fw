d7072f2c07f33881efa4afa26da1ecb68ecc8e90
Yuan Wang <bg3mdo@gmail.com>
2018-09-03 13:08:59 +0100

    Init commit and push

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-bg3mdo-mmdvm-fw
+# bg3mdo-mmdvm-fw updater
+
+1.Current Version; 1.4.7
+2.You might need to adjust TRX offse under MMDVMhost ini/expert mode in Pi-star.
+
+DE BG3MDO, M6IDO
+
+How to run, copy and paste the following command into terminal.
+
+rpi-rw&&wget https://github.com/bg3mdo/bg3mdo-mmdvm-fw/raw/master/update.sh&&chmod +x update.sh&&./update.sh
+
+
+
+
---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo mount -o remount,rw /
+#Install Required Software
+wget https://github.com/juribeparada/MMDVM_HS/releases/download/v1.4.7/mmdvm_hs_hat_fw.bin
+# DV-Mega Mods
+sudo systemctl stop mmdvmhost.timer
+sudo systemctl stop mmdvmhost.service
+sudo stm32flash -v -w mmdvm_hs_hat_fw.bin -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
+sudo systemctl start mmdvmhost.timer
+sudo systemctl restart mmdvmhost.service
+sudo rm mmdvm_hs_hat_fw.bin
+sudo rm update.sh
+sudo mount -o remount,ro /
